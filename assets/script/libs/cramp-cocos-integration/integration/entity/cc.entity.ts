ee80908fb20a703ba1f2e5496a62e3de3bcb7b36
dstolpnyk <d.stolpnyk@twinwingames.com>
2021-01-27 22:44:41 +0200

    Change caching components behaviour

--- assets/script/libs/cramp-cocos-integration/integration/entity/cc.entity.ts
+++ assets/script/libs/cramp-cocos-integration/integration/entity/cc.entity.ts
@@ -1,8 +1,9 @@
-import BaseDeletingComponentBehaviour from "../../cramp/core/entity/entity-behaviours/base-deleting-component.behaviour";
+import ComponentsCache from "../../cramp/cache/components.cache";
 import BaseGettingComponentBehaviour from "../../cramp/core/entity/entity-behaviours/base-getting-component.behaviour";
+import CachedComponentsAddingComponentBehaviour from "../../cramp/core/entity/entity-behaviours/cached-components-adding-component.behaviour";
+import CachedComponentsDeletingComponentBehaviour from "../../cramp/core/entity/entity-behaviours/cached-components-deleting-component.behaviour";
 import { IEntityBehaviour, IEntityComponentManipulationBehaviour } from "../../cramp/type-definitions/interfaces";
 import { ComponentConstructor } from "../../cramp/type-definitions/types";
-import CocosCreatorAddingComponentBehaviour from "../../integration/entity/cocos-creator-adding-component.behaviour";
 import { ICocosCreatorEntity } from "../../types/cc-intergration.interfaces";
 import CocosCreatorCachedComponent from "../component/cc.cached.component";
 
@@ -17,18 +18,18 @@ export default class CocosCreatorEntity extends cc.Component
 {
     
     private _components: cc.Component[] = [];
-    private _cache: cc.Component[] = [];
+    private _cache: ComponentsCache<cc.Component> = new ComponentsCache();
 
     private _componentGettingBehaviour: IEntityComponentManipulationBehaviour<cc.Component, ICocosCreatorEntity> 
         = new BaseGettingComponentBehaviour();
 
     private _componentAddingBehaviour: IEntityComponentManipulationBehaviour<cc.Component, ICocosCreatorEntity>
-        = new CocosCreatorAddingComponentBehaviour(this);
+        = new CachedComponentsAddingComponentBehaviour(this._cache);
 
     private _componentDeletingBehaviour: IEntityComponentManipulationBehaviour<cc.Component, ICocosCreatorEntity>
-        = new BaseDeletingComponentBehaviour();
+        = new CachedComponentsDeletingComponentBehaviour(this._cache);
 
-    public get cache(): cc.Component[] { return this._cache; }
+    public get cache(): cc.Component[] { return this._cache.components; }
     public get components(): cc.Component[] { return this._components; }
     public get isActive(): boolean { return this.node.active; }
     public set isActive(value: boolean) { this.node.active = value; }
@@ -68,12 +69,14 @@ export default class CocosCreatorEntity extends cc.Component
     }
 
     public add<T extends cc.Component>(componentConstructor: ComponentConstructor<T>): T {
-        return this._componentAddingBehaviour.execute(this, componentConstructor) as T;
+        const component: T = this._componentAddingBehaviour.execute(this, componentConstructor);
+        this._toggleCacheStatus(component, false);
+        return component;
     }
 
     public remove<T extends cc.Component>(componentConstructor: ComponentConstructor<T>): T {
         const component = this._componentDeletingBehaviour.execute(this, componentConstructor) as T;
-        this._cache.push(component);
+        this._toggleCacheStatus(component, true);
         return component;
     }
 
@@ -84,7 +87,11 @@ export default class CocosCreatorEntity extends cc.Component
             : !component.enabled
         );
 
-        cachedComponents.forEach(cachedComponent => this.remove(cachedComponent.constructor.prototype.constructor));
+        cachedComponents.forEach(c => this.remove(c.constructor.prototype.constructor));
+    }
+
+    private _toggleCacheStatus(component: cc.Component, inCache: boolean): void {
+        component instanceof CocosCreatorCachedComponent ? component.inCache  = inCache : component.enabled = !inCache;
     }
 
 }
