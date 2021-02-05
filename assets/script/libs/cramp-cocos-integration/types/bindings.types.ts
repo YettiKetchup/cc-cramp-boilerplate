0dd09b92627b31482fbdc08c6d6702c26ffc0005
dstolpnyk <d.stolpnyk@twinwingames.com>
2021-02-05 10:19:41 +0200

    Add type bindings

---
+++ assets/script/libs/cramp-cocos-integration/types/bindings.types.ts
@@ -0,0 +1,11 @@
+import CocosCreatorCachedComponent from '../integration/component/cc.cached.component';
+import CocosCreatorEntity from '../integration/entity/cc.entity';
+import CocosCreatorSystem from '../integration/system/cc.system';
+
+
+
+export class            CrampComponent extends          cc.Component {};
+export class            CrampCachedComponent extends    CocosCreatorCachedComponent {};
+export class            CrampNode extends               cc.Node {};
+export abstract class   CrampSystem<TData> extends      CocosCreatorSystem<TData> {};
+export class            CrampEntity extends             CocosCreatorEntity {};
