67582838e6fc3d1949a58eaa46d8684c2a36cc7b
dstolpnyk <d.stolpnyk@twinwingames.com>
2021-02-05 11:24:35 +0200

    Pretify code of bindings

--- assets/script/libs/cramp-cocos-integration/types/bindings.types.ts
+++ assets/script/libs/cramp-cocos-integration/types/bindings.types.ts
@@ -4,8 +4,8 @@ import CocosCreatorSystem from '../integration/system/cc.system';
 
 
 
-export class            CrampComponent extends          cc.Component {};
-export class            CrampCachedComponent extends    CocosCreatorCachedComponent {};
-export class            CrampNode extends               cc.Node {};
-export abstract class   CrampSystem<TData> extends      CocosCreatorSystem<TData> {};
-export class            CrampEntity extends             CocosCreatorEntity {};
+export class            CrampComponent          extends     cc.Component {};
+export class            CrampCachedComponent    extends     CocosCreatorCachedComponent {};
+export class            CrampNode               extends     cc.Node {};
+export abstract class   CrampSystem<TData>      extends     CocosCreatorSystem<TData> {};
+export class            CrampEntity             extends     CocosCreatorEntity {};
