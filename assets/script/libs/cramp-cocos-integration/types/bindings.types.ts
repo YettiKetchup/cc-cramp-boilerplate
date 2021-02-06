2ba50787e7e946482d3935bde5c8d3eaa5b7c25b
dstolpnyk <d.stolpnyk@twinwingames.com>
2021-02-06 11:35:52 +0200

    Add EntityFactory to type bindings

--- assets/script/libs/cramp-cocos-integration/types/bindings.types.ts
+++ assets/script/libs/cramp-cocos-integration/types/bindings.types.ts
@@ -1,3 +1,4 @@
+import { IEntityFactory } from '../cramp/type-definitions/interfaces';
 import CocosCreatorCachedComponent from '../integration/component/cc.cached.component';
 import CocosCreatorEntity from '../integration/entity/cc.entity';
 import CocosCreatorSystem from '../integration/system/cc.system';
@@ -9,3 +10,7 @@ export class            CrampCachedComponent    extends     CocosCreatorCachedCo
 export class            CrampNode               extends     cc.Node {};
 export abstract class   CrampSystem<TData>      extends     CocosCreatorSystem<TData> {};
 export class            CrampEntity             extends     CocosCreatorEntity {};
+
+export abstract class   CrampEntityFactory<TData>      implements   IEntityFactory<CrampComponent, CrampEntity, TData> {
+    public abstract create(id: string, data?: TData): CrampEntity;
+}
